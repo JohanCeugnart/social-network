63161bca6ae70dec3a92a8ab9a097ebef3cd117f
Louis Allard <la.devweb@yahoo.com>
2021-10-20 11:17:34 +0200

    Add txt.

--- pages/header.js
+++ pages/header.js
@@ -1,11 +1,13 @@
 import styles from '../styles/Header.module.css'
 import Image from 'next/image'
 import logo from '../public/logo.png'
+import { top } from '../lng/fr.js'
 
 export default function header() {
     return (
         <div className={styles.dataContainer}>
-            <Image className={styles.logo} src={logo} alt="logo"/>
+            <Image className={styles.logo} src={logo} alt="logo"/><br />
+            <div className={styles.txt}>{ top.txt }</div>
         </div>
     )
  }
